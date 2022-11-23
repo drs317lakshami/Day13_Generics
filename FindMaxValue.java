ad0a25aeee75c58eeeb596a4e169b9c508b67a15
drs317lakshami <devrajkumar317@gmail.com>
2022-11-23 21:31:35 +0530

    Added :finding max value of Strings

--- FindMaxValue.java
+++ FindMaxValue.java
@@ -1,19 +1,27 @@
 package com.bridgelabz.shahazad;
 
 public class FindMaxValue {
+	
 
-    public static Float maxOfInteger(Float Value1, Float Value2, Float Value3){
-    	Float max = Value1;
-        if (Value2.compareTo(max) > 0)
-            max = Value2;
-        if (Value3.compareTo(max) > 0)
-            max = Value3;
-        return max;
-    }
+	public static String toFindMax(String  String1,String String2,String String3) {
+		
+		String max = String1;
+		if (String2.compareTo(max) > 0 ) {
+			max = String2;
+		}
+		if (String3.compareTo(max) > 0) {
+			max = String3;
+		}
+		
+		return max;
+		
+	}
 
-    public static void main(String[] args) {
-    	
-        System.out.println("Welcome To Find Maximun Problem Using Generics \n");
-        
-        System.out.println("The maximum value between the three integer is : " + maxOfInteger(3.5f, 8.5f, 5.5f));
-    }}
+	public static void main(String[] args) {
+		
+		FindMaxValue maximum = new FindMaxValue();
+		System.out.println("Maximum of among 3 Values is "+toFindMax("Apple", "Peach", "Banana"));
+
+	}
+
+}
