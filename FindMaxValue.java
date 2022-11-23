a6615f97c7cec72f750a2b66a65946c6ebb4fe93
drs317lakshami <devrajkumar317@gmail.com>
2022-11-23 09:43:11 +0530

    Added :finding max float number from 3.5, 8.5, 5.5

--- FindMaxValue.java
+++ FindMaxValue.java
@@ -2,8 +2,8 @@ package com.bridgelabz.shahazad;
 
 public class FindMaxValue {
 
-    public static Integer maxOfInteger(Integer Value1, Integer Value2, Integer Value3){
-        Integer max = Value1;
+    public static Float maxOfInteger(Float Value1, Float Value2, Float Value3){
+    	Float max = Value1;
         if (Value2.compareTo(max) > 0)
             max = Value2;
         if (Value3.compareTo(max) > 0)
@@ -12,7 +12,8 @@ public class FindMaxValue {
     }
 
     public static void main(String[] args) {
+    	
         System.out.println("Welcome To Find Maximun Problem Using Generics \n");
         
-        System.out.println("The maximum value between the three integer is : " + maxOfInteger(3, 8, 5));
+        System.out.println("The maximum value between the three integer is : " + maxOfInteger(3.5f, 8.5f, 5.5f));
     }}
